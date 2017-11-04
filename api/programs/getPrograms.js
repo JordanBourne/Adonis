2835f2cb94ad57aae413c427a7cac5af99075ce3
Jordan Bourne <jordanbourne@ymail.com>
2017-11-04 01:57:04 -0400

    Changed some things

--- api/programs/getPrograms.js
+++ api/programs/getPrograms.js
@@ -2,9 +2,7 @@ const sdk = require('../../sdk');
 const ListPrograms = sdk.programs.listPrograms;
 
 module.exports = function(req, res, next) {
-	ListPrograms.create({
-		filter: null
-	}).execute((err, programs) => {
+	ListPrograms.create().execute((err, programs) => {
 		if(err) {
 			console.log(err);
 			return next(err);
